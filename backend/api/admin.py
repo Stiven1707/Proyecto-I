3347992c9e83ab92131d157f596cbd2eacfc1321
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-08-26 16:30:50 -0500

    ajuste y modelo user

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from api.models import User, Profile
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'bio', 'verified')
+    list_filter = ('verified',)
+    list_editable = ('verified',)
+    search_fields = ('user', 'full_name')
+    ordering = ('user',)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
