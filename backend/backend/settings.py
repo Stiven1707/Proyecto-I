b646dc653b33ae1d1d8aaef03468daabadc6586a
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-08-26 17:08:56 -0500

    otros ajuste jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,10 +133,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLWED_ORIGINS = []
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',)
+    ,
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
