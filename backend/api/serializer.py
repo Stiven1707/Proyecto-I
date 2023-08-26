be3f812f387debb4d016f024791b2ac39ead230b
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-08-26 16:43:11 -0500

    vista y rutas

---
+++ backend/api/serializer.py
@@ -0,0 +1,45 @@
+from .models import User, Profile
+from django.contrib.auth.password_validation import  validate_password
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['full_name'] = user.profile.full_name
+
+        return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Los campos de contraseña no coinciden."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Profile, User
+from .serializer import UserSerializer, MyTokenObtainPairSerializer, RegisterSerializer
+from rest_framework import generics, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+# Create your views here.
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = ([AllowAny])
+    serializer_class = RegisterSerializer
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def dashboard(request):
+    if request.method == 'GET':
+        response = f"Hello, {request.user}, you are seeing a GET request"
+        return Response({'response': response}, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        text = request.data.get('text')
+        response = f"Hello, {request.user}, you are seeing a POST request, and you sent this text: {text}"
+        return Response({'response': response}, status=status.HTTP_200_OK)
+    return Response({}, status=status.HTTP_400_BAD_REQUEST)
