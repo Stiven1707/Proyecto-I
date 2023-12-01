ff17e352043bf7fb35a3d47265a61935edbc24f7
Mditran <micheldiaz@unicauca.edu.co>
2023-12-01 16:48:20 -0500

    Se agrego el filtro en el modulo de revision de anteproyectos

--- frontend/src/Components/Views/PropuestaTesisAuxiliar/index.jsx
+++ frontend/src/Components/Views/PropuestaTesisAuxiliar/index.jsx
@@ -178,25 +178,25 @@ const PropuestaTesisAuxiliar = () => {
                                     }}>Buscar</button>
                                 </div>
                             </div>
-                        <div className='flex items-center'>
-                                        <label htmlFor="Filtros" className="block mr-3 mb-2 text-sm font-medium text-gray-900 dark:text-white">FILTRO</label>
-                                        <select
-                                                name="filtro"
-                                                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                                                value={filtro}
-                                                onChange={(e)=>{
-                                                    const { value } = e.target
-                                                    setFiltro(value)
-                                                    getPropuestas()
-                                                }}
-                                                required 
-                                                >
-                                                <option value='PENDIENTE'>Pendiente</option>
-                                                <option value='APROBADO'>Aprobado</option>
-                                            </select>
-                                    </div>
+                            <div className='flex items-center'>
+                                <label htmlFor="Filtros" className="block mr-3 mb-2 text-sm font-medium text-gray-900 dark:text-white">FILTRO</label>
+                                <select
+                                    name="filtro"
+                                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                    value={filtro}
+                                    onChange={(e)=>{
+                                        const { value } = e.target
+                                        setFiltro(value)
+                                        getPropuestas()
+                                    }}
+                                    required 
+                                    >
+                                    <option value='PENDIENTE'>Pendiente</option>
+                                    <option value='APROBADO'>Aprobado</option>
+                                </select>
+                            </div>
                         </div>
-                </div>
+                    </div>
                 <table className="sticky w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
