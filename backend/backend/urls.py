9486808313269ea785ba1f7f086ca18afce2ae48
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-12-02 23:23:37 -0500

    Add API documentation endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,7 +24,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), # new
-    path('docs/', include_docs_urls(title='Documentación API', public=True)), # new
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('docs/', include_docs_urls(title='Documentación API', public=True)), ]
+
